c796eabbab0234a94f33390d8906b95c190b5e7f
Ольга Жолудева <joludeva.olga@yandex.ru>
2023-02-03 18:28:58 +0300

    Переименовала проект

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -3,14 +3,8 @@ from django.contrib import admin
 from .models import User, Category, Genre, Title, Review, Comment
 
 
-class TitleAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'genre', 'year', 'description',)
-    search_fields = ('name',)
-    list_filter = ('category',)
-    empty_value_display = '-пусто-'
 
-
-admin.site.register(Title, TitleAdmin)
+admin.site.register(Title)
 admin.site.register(User)
 admin.site.register(Category)
 admin.site.register(Genre)
